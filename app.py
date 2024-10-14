e47d239aac3ea62cadcaa5fd0e51afb8f86ead7a
Vishnu Suresh Perumbavoor <vishnusureshperumbavoor@gmail.com>
2024-10-14 22:45:05 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from groq import Groq
+import base64
+
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+image_path = "medical.png"
+
+base64_image = encode_image(image_path)
+
+client = Groq()
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "Can you find any abnormalities in this CT scan image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                },
+            ],
+        }
+    ],
+    model="llava-v1.5-7b-4096-preview",
+)
+
+print(chat_completion.choices[0].message.content)
